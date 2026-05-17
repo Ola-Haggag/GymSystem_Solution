ae6bd8cb8a98ca444c98527ce37df0eb9f98cccb
Ola Haggag <143203892+Ola-Haggag@users.noreply.github.com>
2026-05-17 17:28:02 +0300

    Add project files.

---
+++ GymSystem/Contexts/GymDbContext.cs
@@ -0,0 +1,21 @@
+﻿using GymSystem.Configurations;
+using GymSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace GymSystem.Contexts
+{
+    public class GymDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=GymDb;trusted_Connection=true;trustServerCertificate=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Plan>(new PlanConfigurations());
+        }
+        public DbSet<Plan> Plans { get; set;}
+    }
+}
---
+++ GymSystem/Controllers/PlanController.cs
@@ -0,0 +1,27 @@
+﻿using GymSystem.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymSystem.Controllers
+{
+    public class PlanController : Controller
+    {
+        //2 Actions :Index(All Plans) Details(Plan Details)
+        
+        private GymDbContext dbContext = new GymDbContext();
+        public async Task<IActionResult> Index()
+        {
+            var plans =await dbContext.Plans.ToListAsync();
+            return View(plans); // view with model
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var plan = await dbContext.Plans.FirstOrDefaultAsync(p=>p.Id == id);
+            if (plan == null)
+                RedirectToAction(nameof(Index));
+            
+            return View(plan);
+        }
+    }
+}
